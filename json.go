640f444c92deb1bb805ef827a865074bf8e9455d
arvind chaudhary <arvindchaudhary@MacBook-Pro.local>
2023-08-18 15:50:46 +0530

    small change

--- json.go
+++ json.go
@@ -9,7 +9,7 @@ import (
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}){
 	data, err := json.Marshal(payload)
 	if err != nil {
-		log.Printf("Falied to marshal JSON response: %v", payload)
+		log.Printf("Failed to marshal JSON response: %v", payload)
 		w.WriteHeader(500)
 		return
 	}
